29fdb7be2b17eb5771c91d6b6623c9559eb0f5dd
Farwa Syed <farwa.asyed@gmail.com>
2019-03-25 23:32:08 +1100

    styled

--- src/App.js
+++ src/App.js
@@ -21,36 +21,45 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
-    console.log(data);
-     this.setState({
-       temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        error: ""
-     });
-  }
+    if (city && country) {
+          this.setState({
+            temperature: data.main.temp,
+            city: data.name,
+            country: data.sys.country,
+            humidity: data.main.humidity,
+            description: data.weather[0].description,
+            error: ""
+          });
+        } else {
+          this.setState({
+            temperature: undefined,
+            city: undefined,
+            country: undefined,
+            humidity: undefined,
+            description: undefined,
+            error: "Please enter the values."
+          });
+        }
+      }
 
 
-  render(){
-    return (
-      <div>
-        <h2>Check Weather Component</h2>
-        <Titles />
-        <Form getWeather={this.getWeather}/>
-        <Weather
-          temperature={this.state.temperature}
-          humidity={this.state.humidity}
-          city={this.state.city}
-          country={this.state.country}
-          description={this.state.description}
-          error={this.state.error}
-                  />
-
-      </div>
-    );
-  }
-};
-
+      render() {
+        return (
+          <div>
+            <div className="wrapper">
+                      <Titles />
+                    </div>
+                      <Form getWeather={this.getWeather} />
+                      <Weather
+                        temperature={this.state.temperature}
+                        humidity={this.state.humidity}
+                        city={this.state.city}
+                        country={this.state.country}
+                        description={this.state.description}
+                        error={this.state.error}
+                      />
+          </div>
+        );
+      }
+    };
 export default App;
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,8 +4,8 @@ class Titles extends React.Component {
   render(){
     return(
       <div>
-        <h3>Weather Finder</h3>
-        <p>Find out temperature</p>
+      <h1 className="title-container__title">Check Weather</h1>
+      <h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
       </div>
     );
   }
