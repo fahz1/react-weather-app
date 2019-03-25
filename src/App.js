79f609348d758eb7f1cdc2c907a9a46d6003010e
Farwa Syed <farwa.asyed@gmail.com>
2019-03-25 20:30:45 +1100

    fixed the typo

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ const API_KEY = "41ae6590cc803c925d9d02205539a03a";
 
 class App extends React.Component {
   getWeather = async (e) => {
-    e.prevenDefault();
-    // const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
-     const data = await api_call.json();
+    e.preventDefault();
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
     console.log(data);
   }
+
+
 
   render(){
     return (
