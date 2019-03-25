d6db37cf2e751627666672389955df4b0def40d5
Farwa Syed <farwa.asyed@gmail.com>
2019-03-25 19:07:28 +1100

    typo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   getWeather = async (e) => {
     e.prevenDefault();
     // const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-    const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
      const data = await api_call.json();
     console.log(data);
   }
